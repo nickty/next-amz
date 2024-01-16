38106f1b02f0e817241a5bfac027263bce05dd09
Mizanur Rahman <nickty.86@gmail.com>
2024-01-16 08:46:22 +0600

    product model on the way

---
+++ components/header/products/ProductItem.tsx
@@ -0,0 +1,6 @@
+import { Product } from '@/lib/models/ProductModel'
+import React from 'react'
+
+export default function ProductItem({ product }: { product: Product }) {
+  return <div>ProductItem</div>
+}

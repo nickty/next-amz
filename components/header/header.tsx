3c7b51f757b7a01f04d66ea74acbade1552b79b6
Mizanur Rahman <nickty.86@gmail.com>
2024-01-16 08:22:58 +0600

    navigation setup

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Mizan Next Amz
+          </Link>
+          <ul className="flex">
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/cart">
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/signin">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header

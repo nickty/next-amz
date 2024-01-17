4edc78d4775bd2742aeaa4472e48045d953571ac
Mizanur Rahman <nickty.86@gmail.com>
2024-01-17 08:58:14 +0600

    demo product items added

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,5 +1,16 @@
+import ProductItem from '@/components/header/products/ProductItem'
+import data from '@/lib/data'
 import Image from 'next/image'
 
 export default function Home() {
-  return <h2 className="text-2xl py-2">Latest products</h2>
+  return (
+    <>
+      <h2 className="text-2xl py-2">Latest products</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.slug} product={product} />
+        ))}
+      </div>
+    </>
+  )
 }
